b880a5ca4ec217f29b0f63a3c524e8a339146eee
mas454 <masahiro454@gmail.com>
2009-11-26 13:57:51 +0900

    関数を定義できるようにした

--- compile.scm
+++ compile.scm
@@ -4,20 +4,27 @@
 (define (program-list-compile code-list asm-list)
   (if (null? code-list)
       (append '((putnil))(reverse asm-list))
-      (let ((asm (compile (car code-list))))
+      (let ((asm (compile (car code-list) #f)))
 	(program-list-compile (cdr code-list) (cons asm asm-list)))))
 
-(define (compile code)
+(define (compile code meth-argp)
   (cond 
    ((object? code) `(putobject ,code))
    ((symbol? code) `(getlocal ',code))
    ((=? code)
     (set! symbol-list (cons (cadr code) symbol-list))
-    `(,(compile (caddr code)) (setlocal ',(cadr code))))
+    `(,(compile (caddr code) meth-argp) (setlocal ',(cadr code))))
+   ((def? code)
+    `(def ',(cdr code)))
    ((run? code)
-    (append
-     (args-compile (cdr code) '())     
-	  `((call ',(car code) ,(length (cdr code))))))
+    (if meth-argp
+	(append
+	 '((putnil))
+	 (args-compile (cdr code) '())     
+	 `((call ',(car code) ,(length (cdr code)))))
+	(append
+	 (args-compile (cdr code) '())     
+	 `((call ',(car code) ,(length (cdr code)))))))
    (else
     (error code))))
 
@@ -28,14 +35,14 @@
   (if (null? code)
       (reverse arg-list)
       (args-compile (cdr code)
-		    (cons (compile (car code)) arg-list))))
+		    (cons (compile (car code) #t) arg-list))))
 
 
 (define (dprint . lis)
   (letrec ((print-list (lambda (lis)
 			 (if (not (null? lis))
 			     (begin
-			       (display (car lis))
+			       (display (car lis) out-p)
 			       (print-list (cdr lis)))))))
     (print-list lis)))
 
@@ -44,6 +51,9 @@
 
 (define (=? exp)
   (tagged-list? exp '=))
+
+(define (def? exp)
+  (tagged-list? exp 'def))
 
 (define (tagged-list? exp tag)
   (if (pair? exp)
@@ -57,13 +67,10 @@
 			      (dprint atom)
 			      (dprint "\""))
 			     (else
-			      (dprint atom))))))
-    
-			      
-			      
+			      (dprint atom))))))		      
     (dprint "putobject ")
     (print-obj atom)
-    (newline)))
+    (dprint "\n")))
 (define (getlocal sym)
   (dprint "getlocal :" sym "\n"))
 
@@ -71,23 +78,35 @@
   (dprint "setlocal :" sym "\n"))
 
 (define (call fn len)
-  (dprint "call :")
-  (dprint fn)
-  (dprint ",")
-  (dprint len)
-  (newline))
+  (dprint "call :" fn ", " len "\n"))
 
 (define (putnil)
-  (dprint "putnil")
-  (newline))
+  (dprint "putnil\n"))
 
 (define (asm-print asm)
- (dprint asm)
- (newline))
+ (dprint asm "\n"))
+
+(define (def code)
+  (dprint "definemethod(:" (car code) ",") 
+  (method-compile-print code)
+  (dprint ")\n"))
+
+(define (method-compile-print code)
+  (define temp symbol-list)
+  (set! symbol-list (cadr code))
+  (let ((asm-list (program-list-compile (cddr code) '())))
+    (dprint "YASM.method(:" (car code) ", [")
+    (symbol-list-print symbol-list)
+    (dprint "]){\n")
+    (compile-print asm-list)
+    (dprint "leave\n" "}"))
+  (set! symbol-list temp))
 
 (define symbol-list '())
 (define (loy-compile code-list)
     (let ((asm-list (program-list-compile code-list '())))
+      (dprint "require \'yasm\'\n")
+      (dprint "require \'lispu\'\n")
       (dprint "iseq = YASM.toplevel([")
       (symbol-list-print symbol-list)
       (dprint "]){\n")
@@ -95,11 +114,12 @@
       (dprint "leave\n" "}\n iseq.eval\n")))
   
 (define (symbol-list-print sym-lis)
-  (if (not (null? (cdr sym-lis)))
-      (begin
-	(dprint ":" (car sym-lis) ",")
-	(symbol-list-print (cdr sym-lis)))
-      (dprint ":" (car sym-lis))))
+  (if (not (null? sym-lis))
+      (if (not (null? (cdr sym-lis)))
+	  (begin
+	    (dprint ":" (car sym-lis) ",")
+	    (symbol-list-print (cdr sym-lis)))
+	  (dprint ":" (car sym-lis)))))
       
 (define (compile-print asm-list)
   (if (not (null? asm-list))
@@ -108,6 +128,8 @@
 	    (begin
 	      (compile-print (car asm-list))
 	      (compile-print (cdr asm-list))))))
-
-(loy-compile '((= x 20) (puts x)))
+(define out-p (open-output-file "c-test.rb"))
+(loy-compile '((= a 10) (= b 20) (puts (+ a (+ b 20)))))
+;(asm-print (compile '(def abs (a)
+                      ; (+ a 10))))
 ;(loy-compile '(puts 10))
