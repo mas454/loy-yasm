f57095f16947c85f75da91d0ec45b0bd6d1c9601
mas454 <masahiro454@gmail.com>
2009-11-26 10:34:42 +0900

    メソッドの実行ができるようになった

--- compile.scm
+++ compile.scm
@@ -5,25 +5,72 @@
   (cond 
    ((object? code) `(putobject ,code))
    ((symbol? (car code))
-    (append (args-compile (cdr code) '())     
-	  `((call ,(car code) ,(length (cdr code))))))))
-
+    (append
+     '((putnil))
+     (args-compile (cdr code) '())     
+	  `((call ',(car code) ,(length (cdr code))))))))
+(define (
 (define (args-compile code arg-list)
   (if (null? code)
       (reverse arg-list)
       (args-compile (cdr code)
 		    (cons (compile (car code)) arg-list))))
+(define (dprint a)
+  (display a))
 (define (atom? a)
-  (not (list? a)))
-(define (putobject num)
-  (display "putobject ")
-  (display num)
-  (newline))
+  (not (pair? a)))
 
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (putobject atom)
+  (letrec ((print-obj (lambda (atom)
+		       (cond ((string? atom)
+			      (dprint "\"")
+			      (dprint atom)
+			      (dprint "\""))
+			     (else
+			      (dprint atom))))))
+    
+			      
+			      
+    (dprint "putobject ")
+    (print-obj atom)
+    (newline)))
+
+
+(define (call fn len)
+  (dprint "call :")
+  (dprint fn)
+  (dprint ",")
+  (dprint len)
+  (newline))
+(define (putnil)
+  (dprint "putnil")
+  (newline))
 (define (asm-print asm)
- (display asm)
+ (dprint asm)
  (newline))
 
-(eval '(putobject 10) (interaction-environment))
-;(asm-print (compile '(puts (+ 10 20))))
-;(asm-print (compile '(puts (+ 10 20))))
+
+(define (loy-compile code)
+  (let ((asm-list (compile code)))
+	(dprint "iseq = YASM.toplevel([]){\n")
+	(compile-print asm-list)
+	(dprint "leave\n")
+	(dprint "}\n iseq.eval\n")))
+  
+
+(define (compile-print asm-list)
+  (if (not (null? asm-list))
+      (if (symbol? (car asm-list))
+	    (eval asm-list (interaction-environment))
+	    (begin
+	      (compile-print (car asm-list))
+	      (compile-print (cdr asm-list))))))
+
+;(compile-print (compile '(puts (+ 10 20))))
+(loy-compile '(puts 10))
+;(asm-print (compile '(puts 10)))
