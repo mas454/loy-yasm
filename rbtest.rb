56e3b3f0a0b1d722d946212a7baffad5b2e3bb12
mas454 <masahiro454@gmail.com>
2009-12-16 23:14:40 +0900

    コンパイルできた

--- rbtest.rb
+++ rbtest.rb
@@ -1,7 +1,7 @@
 require("lispu.rb")
 #puts "\"abc\"".to_sym.equal?(:abc)
 puts "print-scm".to_sym
-
+puts ARGV[0].kind_of?(String)
 
 #puts RubyVM::InstructionSequence.compile_file("rbtest.rb").disasm
 
