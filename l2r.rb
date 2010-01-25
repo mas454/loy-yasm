a3d8a3762e58dad4463d5aaab3119c428f55fc00
mas454 <masahiro454@gmail.com>
2010-01-25 10:06:23 +0900

    asmマクロを書いた

--- l2r.rb
+++ l2r.rb
@@ -7,7 +7,7 @@ str_list[0] + string_list_append(str_list[1])
 end
 end
 
-
+nil
 def map(fn, lis)
 if null?(lis)
 nil
@@ -16,22 +16,22 @@ else
 end
 end
 
-
+nil
 def object?(code)
 number?(code) or eq?(nil, code) or symbol?(code) or eq?(true, code) or eq?(false, code)
 end
 
-
+nil
 def run?(code)
 pair?(code) & symbol?(code[0])
 end
 
-
+nil
 def set?(exp)
 tagged_list?(exp, :"=")
 end
 
-
+nil
 def tagged_list?(exp, tag)
 if pair?(exp)
 eq?(exp[0], tag)
@@ -40,12 +40,12 @@ false
 end
 end
 
-
+nil
 def cons?(code)
 tagged_list?(code, :"cons")
 end
 
-
+nil
 def tostr(code)
 code.to_s
 end
@@ -58,46 +58,17 @@ else
 end
 end
 
-
-def caar(code)
-code[0][0]
-end
-
-
-def cdar(code)
-code[0][1]
-end
-
-
-def cddr(code)
-code[1][1]
-end
-
-
-def cadr(code)
-code[1][0]
-end
-
-
-def caddr(code)
-code[1][1][0]
-end
-
-
-def cdadr(code)
-code[1][0][1]
-end
-
-
+nil
 def wqu()
 "\""
 end
 
+nil
 def macro_val_print?(code)
 tagged_list?(code, :"macro_val_print")
 end
 
-
+nil
 def compile(code, argp)
 if string?(code)
 wqu() + code + wqu() + argp_print(argp)
@@ -174,14 +145,14 @@ exit(1)
 end
 end
 
-
+nil
 $macro_list = nil
-
+nil
 def define_macro?(exp)
 tagged_list?(exp, :"macro")
 end
 
-
+nil
 def macro?(exp)
 if pair?(exp)
 memq(exp[0], $macro_list)
@@ -190,7 +161,7 @@ false
 end
 end
 
-
+nil
 def macro_compile(code, argp)
 "" + code[0] + if null?(code[1])
 "()" + argp_print(argp)
@@ -199,7 +170,7 @@ else
 end
 end
 
-
+nil
 def run_compile(code, argp)
 "" + code[0] + if null?(code[1])
 "()" + argp_print(argp)
@@ -212,42 +183,42 @@ end
 end
 end
 
-
-
-
-
+nil
+nil
+nil
+nil
 def infix?(exp)
-memq(exp[0], [:"+",[:"-",[:"*",[:"/",[:"%",[:"**",[:"^",[:"&",[:"or",]]]]]]]]])
+memq(exp[0], [:"+",[:"-",[:"*",[:"/",[:"%",[:"**",[:"^",[:"&",[:"or",nil]]]]]]]]])
 end
 
-
+nil
 def begin?(exp)
 tagged_list?(exp, :"begin")
 end
 
-
+nil
 def rasm?(exp)
 tagged_list?(exp, :"rasm")
 end
 
-
+nil
 def infix_compile(inf, arg_list, argp)
 compile(arg_list[0], true) + string_list_append(map(lambda {|arg|
 " " + inf + " " + compile(arg, true)
 }, arg_list[1])) + argp_print(argp)
 end
 
-
+nil
 def binfix?(exp)
-memq(exp[0], [:"<",[:">",[:"<=",[:"==",[:"=~",]]]]])
+memq(exp[0], [:"<",[:">",[:"<=",[:"==",[:"=~",nil]]]]])
 end
 
-
+nil
 def binfix_compile(inf, arg1, arg2, argp)
 compile(arg1, true) + " " + inf + " " + compile(arg2, argp)
 end
 
-
+nil
 def def_arg_string(lis)
 arg_comp = lambda {|arg|
 if symbol?(arg)
@@ -265,7 +236,7 @@ else
 end
 end
 
-
+nil
 def def_compile(code)
 if symbol?(code[0])
 "def " + code[0] + "(" + def_arg_string(cadr(code)) + ")\n" + program_list_compile(cddr(code)) + "end\n\n"
@@ -274,7 +245,7 @@ else
 end
 end
 
-
+nil
 def macro_args_compile(lis)
 compile(quote_compile(lis[0]), true) + if null?(lis[1])
 ""
@@ -285,8 +256,8 @@ string_list_append(map(lambda {|a|
 end
 end
 
-
-
+nil
+nil
 def quote_compile(lis)
 if null?(lis)
 nil
@@ -301,12 +272,12 @@ list(:"cons", quote_compile(lis[0]), quote_compile(lis[1]))
 end
 end
 
-
+nil
 def lambda?(code)
 tagged_list?(code, :"lambda") or tagged_list?(code, :"->")
 end
 
-
+nil
 def lambda_compile(lam_list, argp)
 "lambda {|" + def_arg_string(lam_list[0]) + "|\n" + program_list_compile(lam_list[1]) + "}" + argp_print(argp)
 end
@@ -315,17 +286,17 @@ def block_compile(bl_list)
 " {|" + def_arg_string(bl_list[0]) + "|\n" + program_list_compile(bl_list[1]) + "}\n"
 end
 
-
+nil
 def quote?(exp)
 tagged_list?(exp, :"quote")
 end
 
-
+nil
 def if?(exp)
 tagged_list?(exp, :"if")
 end
 
-
+nil
 def if_compile(code, argp)
 "if " + compile(code[0], false) + compile(cadr(code), false) + if null?(cddr(code))
 ""
@@ -334,7 +305,7 @@ else
 end + "end" + argp_print(argp)
 end
 
-
+nil
 def args_cdr_compile(args_list)
 if null?(args_list)
 ""
@@ -343,7 +314,7 @@ else
 end
 end
 
-
+nil
 def args_compile(args_list, argp)
 "(" + compile(args_list[0], true) + string_list_append(map(lambda {|code|
 if tagged_list?(code, :"block")
@@ -360,10 +331,10 @@ end
 }, block?(args_list)) + argp_print(argp)
 end
 
-
+nil
 def block?(lis)
 if null?(lis)
-
+nil
 else
 if tagged_list?(lis[0], :"block")
 cdar(lis)
@@ -373,22 +344,22 @@ end
 end
 end
 
-
+nil
 def car?(code)
 tagged_list?(code, :"car")
 end
 
-
+nil
 def cdr?(code)
 tagged_list?(code, :"cdr")
 end
 
-
+nil
 def cond?(code)
 tagged_list?(code, :"cond")
 end
 
-
+nil
 def cond_compile(code, argp)
 "if " + compile(caar(code), false) + program_list_compile(cdar(code)) + if null?(code[1])
 ""
@@ -417,25 +388,25 @@ cadr(a)
 append2(list(:"lamcall", append2(list(:"lambda", map(car_lam, lis[0])), lis[1])), map(cadr_lam, lis[0]))
 end
 
-
+nil
 def def?(exp)
 tagged_list?(exp, :"def")
 end
 
-
+nil
 def main?(exp)
 tagged_list?(exp, :"main")
 end
 
-
+nil
 def program_list_compile(code_list)
 string_list_append(map(lambda {|a|
 compile(a, false)
 }, code_list))
 end
 
-
-
+nil
+nil
 def quit()
 exit(0)
 end
@@ -449,7 +420,7 @@ if not(define_macro?(exp))
 display(ruby_str, out_file)
 end
 out_file.flush()
-if def?(exp) or define_macro?(exp) or macro?(exp)
+if def?(exp) or define_macro?(exp)
 begin
 eval(ruby_str, TOPLEVEL_BINDING)
 rescue SyntaxError
@@ -514,7 +485,7 @@ end
 }
 end
 
-
+nil
 def main()
 lib_compile()
 if null?(ARGV.get(0))
@@ -528,6 +499,6 @@ end
 end
 end
 
-
+nil
 main()
-
+nil
